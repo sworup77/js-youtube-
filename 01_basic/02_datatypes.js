7d72f01da1892e3d2844e6521636dbbabb7f7251
sworup77 <129973958+sworup77@users.noreply.github.com>
2025-08-25 05:14:53 +0000

    datatypes

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -1,7 +1,7 @@
 // alert(3+3) we are using the node js not a browser 
 let name = "sworup";// string
 let age = 18; // number 
-let isloggedun = true; // booolen 
+let isloggedun = true; // boooleans
 
 
 // number => 2 to power 53
