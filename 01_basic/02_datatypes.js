def5c2ca4a0297478b743c87e8a3a5483089a620
sworup77 <129973958+sworup77@users.noreply.github.com>
2025-08-25 05:13:37 +0000

    datatypes

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -1,1 +1,15 @@
 // alert(3+3) we are using the node js not a browser 
+let name = "sworup";// string
+let age = 18; // number 
+let isloggedun = true; // booolen 
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/fasle
+//null => standalone value
+//undefined =>
+// symbol => unique 
+
+// object 
