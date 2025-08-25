d134fad96240d680dee50460a845f5e6d1074222
sworup77 <129973958+sworup77@users.noreply.github.com>
2025-08-25 09:48:28 +0000

    aaa

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,14 @@
+let score = "33";
+console.log(typeof score);
+let valueinnumber = Number(score);
+// "33" => 33
+
+
+ let islooogedin = 1;
+ let booleanisloggedin = Boolean(islooogedin);
+ console.log(booleanisloggedin);
+
+ let somenumber = 33;
+ let stringnumber = String(somenumber);
+ console.log(stringnumber);
+ console.log(typeof stringnumber);
